059f4df3f20731fa07059acb7946331da3208f83
Oleg <oleg.kishisnkiy@gmail.com>
2022-03-23 14:06:41 +0300

    init

---
+++ src/main/java/com/example/springdemo1/SpringDemo1Application.java
@@ -0,0 +1,15 @@
+package com.example.springdemo1;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemo1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springdemo1/entity/UserEntity.java
@@ -0,0 +1,5 @@
+package com.example.springdemo1.entity;
+
+public class UserEntity {
+
+}
