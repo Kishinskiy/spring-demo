63512250b2dc17c6b76ad0a3e787b45fb78a6c9a
Oleg <oleg.kishinskiy@gmail.com>
2022-03-28 21:50:39 +0300

    change goal

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.freeDiskSpace
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.VcsTrigger
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
@@ -40,15 +42,22 @@ object Test : BuildType({
 
     steps {
         maven {
+            val goalName = "clean package"
             name = "my custom step name"
-            goals = "clean test"
+            goals = goalName
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
 
+
     triggers {
         vcs {
-
+            quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_CUSTOM
+            quietPeriod = 30
+            branchFilter = ""
+            perCheckinTriggering = true
+            groupCheckinsByCommitter = true
+            enableQueueOptimization = false
         }
     }
 })
